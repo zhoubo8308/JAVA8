fbc8441ade0a4d703965850bcf1d7b7bf08df198
zhoubo8308 <zhoubo8308@163.com>
2016-12-08 17:15:00 +0800

    do exercise2

--- src/main/java/stream/Exercise.java
+++ src/main/java/stream/Exercise.java
@@ -1,20 +1,21 @@
 package stream;
 
+import javax.swing.text.html.Option;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalTime;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.*;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 /**
  * Created by bzou on 2016/12/1.
@@ -22,10 +23,24 @@ import java.util.stream.Stream;
 public class Exercise {
 
     public static void main(String ...args)throws Exception{
-        String [] words = {"hello","good", "morning","wow"};
+        String [] words = {"hello","good", "morning","wow","fuckyoujimture"};
       //  System.out.println(getCount_multithreaded(Arrays.asList(words)));
       //  ex2();
-        ex3();
+       // ex3();
+
+       /* long a = 25214903917L;
+        long c = 11L;
+        long m = (long)Math.pow(2,48);
+      ex5(a,c,m,1L);*/
+       // Stream<Double> stream = Stream.generate(Math::random);
+      //  System.out.println(isInfinite(stream));
+
+        /*Double [] temp = {1.0,2.0,3.0,4.0,5.0};
+        List<Double> doubles = Arrays.asList(temp);
+        System.out.println(ex10(doubles.stream()));*/
+
+        //ex12(Stream.of(words));
+        ex13(Stream.of(words));
     }
 
     private static int getCount_multithreaded(List<String> words) throws Exception {
@@ -95,6 +110,93 @@ public class Exercise {
         IntStream intStream2 = Arrays.stream(values);
 
     }
+
+    public static void ex5(Long a, Long c, Long m, Long seed){
+        Stream<Long> longStream = Stream.iterate(seed,n->(a*n+c)%m);
+        System.out.println(longStream.limit(10).collect(Collectors.toList()));
+    }
+
+    /**
+     * It is an improvement for characterStream method in section 2.3
+     * @param s
+     * @return
+     */
+    public static  Stream<Character> ex6(String s){
+       return s.chars().mapToObj(n->(char)n);
+    }
+
+    public static <T> boolean isInfinite(Stream<T> stream){
+        long size = 999999L;
+        if(stream.count() > size){
+            return true;
+        }
+        return  false;
+    }
+
+    public static <T> Stream<T> zip(Stream<T> first, Stream<T> second){
+        return null;
+
+    }
+
+    private static class ZippedIterator<T> implements Iterator<T> {
+        private final Iterator<T> itFirst;
+        private final Iterator<T> itSecond;
+        boolean firstsTurn;
+
+        public ZippedIterator(Iterator<T> itFirst, Iterator<T> itSecond) {
+            this.itFirst = itFirst;
+            this.itSecond = itSecond;
+            firstsTurn = true;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !firstsTurn || (itFirst.hasNext() && itSecond.hasNext());
+        }
+
+        @Override
+        public T next() {
+            T nextVal = firstsTurn ? itFirst.next() : itSecond.next();
+            firstsTurn = !firstsTurn;
+            return nextVal;
+        }
+    }
+
+    //from http://stackoverflow.com/questions/24511052/java8-iterator-to-stream
+    private static <T> Stream<T> asStream(Iterator<T> sourceIterator) {
+        Iterable<T> iterable = () -> sourceIterator;
+        return StreamSupport.stream(iterable.spliterator(), false);
+    }
+
+    public static double ex10(Stream<Double> doubleStream){
+       /* Optional<Double> optional = doubleStream.reduce((x,y)->x+y);
+        //java.lang.IllegalStateException: stream has already been operated upon or closed
+        long count = doubleStream.count();
+        return optional.get()/count;*/
+        Optional<Double> optional = doubleStream.reduce((x,y)->x+y);
+        return 0.0;
+    }
+
+    public static void ex12(Stream<String> words){
+        AtomicInteger[] shortWords = new AtomicInteger[12];
+        for(int i=0;i<shortWords.length;i++){
+            shortWords[i] = new AtomicInteger();
+        }
+        words.parallel().forEach(
+                s -> { if (s.length() < 12) shortWords[s.length()].getAndIncrement(); }
+        );
+        System.out.println(Arrays.toString(shortWords));
+
+    }
+
+    public static void ex13(Stream<String> words){
+        Map<Integer,Long> wordslenght = words.collect(groupingBy(String::length, counting()));
+        wordslenght.forEach((k,v)->{
+            System.out.println("key="+k+"; value="+v);
+        });
+    }
+
+
 
 
 }
